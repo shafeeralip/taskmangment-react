be8c197516377fc320277a79b450ec579076fff5
Shafeerali <shafeeralihp@gmail.com>
2025-05-19 11:10:51 +0530

    Task management react

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const Base_URL = 'http://127.0.0.1:8000'
+const Base_URL = 'https://taskmangement-fastapi-gtb4egenbkayg3f5.canadacentral-01.azurewebsites.net'
 
 export const getTasks = async() =>{
     const res = await fetch(`${Base_URL}/tasks`)
